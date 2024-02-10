cd5edf7beb506bde544e5ede2b0f5ec3fcd34515
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-10 19:22:58 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import { convertMorseCodeToLetters, convertSentenceToMorseCode } from "./utils";
-
-
-
-const data = convertMorseCodeToLetters(". .- -..");
-const data2 = convertSentenceToMorseCode("Halo Aku Adi Kurniawan");
-
-console.log(data,data2);
+export * from "./utils"
